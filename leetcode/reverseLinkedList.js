0eb14b357fc3bf731f54571830bc24a4d43dc603
Mohammad Rafi <rafi.mohammad@autodesk.com>
2022-07-22 17:19:00 +0530

    Added Leetcode 206: Reverse LinkedList

---
+++ leetcode/reverseLinkedList.js
@@ -0,0 +1,24 @@
+// Leetcode: 206 : Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var reverseList = function(head) {
+  let current = head;
+  let prev = null;
+  while (current) {
+      let nextNode = current.next;
+      current.next = prev;
+      prev = current;
+      current = nextNode;
+  }
+  head = prev;
+};
