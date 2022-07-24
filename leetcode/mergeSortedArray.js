3595b7db113d498630449081e196711fce5bdf93
Mohammad Rafi <mohammad.rafi@tothenew.com>
2022-07-24 16:42:51 +0530

    Added Leetcode 88: Merge Sorted Array

---
+++ leetcode/mergeSortedArray.js
@@ -0,0 +1,41 @@
+// Leetcode 88: Merge Sorted Array
+var merge = function (nums1, m, nums2, n) {
+  let first = m - 1;
+  let second = n - 1;
+  let last = m + n - 1;
+  let i = last;
+  for (i = last; i >= 0; i--) {
+    if (first < 0 || second < 0) {
+      break;
+    }
+    if (nums1[first] > nums2[second]) {
+      nums1[i] = nums1[first];
+      first--;
+    } else {
+      nums1[i] = nums2[second];
+      second--;
+    }
+  }
+
+  while (first >= 0) {
+    nums1[i] = nums1[first];
+    first--;
+    i--;
+  }
+
+  while (second >= 0) {
+    nums1[i] = nums2[second];
+    second--;
+    i--;
+  }
+};
+
+let nums1 = [1, 2, 3, 0, 0, 0];
+let nums2 = [2, 5, 6];
+let m = 3;
+let n = 3;
+
+console.log("Nums1 is", nums1);
+console.log("Nums2 is", nums2)
+merge(nums1, m, nums2, n);
+console.log("Merged array is", nums1);
