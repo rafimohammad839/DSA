22e0e9e198fe2a027864a314258c325072b6a04f
Mohammad Rafi <mohammad.rafi@tothenew.com>
2022-07-29 19:18:20 +0530

    changes

--- leetcode/maximumSubarray.js
+++ leetcode/maximumSubarray.js
@@ -1,19 +1,12 @@
-// Leetcode 53. Maximum Subarray
-/**
- * @param {number[]} nums
- * @return {number}
- */
-// ---------------- Optimal Solution - O(N) | Kadane's Algorithm
- var maxSubArray = function(nums) {
-  let max = Number.MIN_SAFE_INTEGER; // Or -Infinity or nums[0]
+// Leetcode 53: Maximum Subarray
+
+// [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+var maxSubArray = function(nums) {
+  let max = Number.MIN_VALUE;
+  let i = 0;
+  let j = 0;
   let curr = 0;
   for (let i = 0; i < nums.length; i++) {
-    if (curr < 0) curr = 0;
-    curr += nums[i];
-    max = Math.max(max, curr);
+    
   }
- return max;
-}; 
-
-let nums = [-1];
-console.log(maxSubArray(nums));
+};
