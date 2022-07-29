6a1c92a3e6137d38341aa6e7a6e5f61fd3e62791
Mohammad Rafi <rafi.mohammad@autodesk.com>
2022-07-29 11:06:25 +0530

    Optimized Leetcode 217: Contains Duplicate

--- leetcode/containsDuplicate.js
+++ leetcode/containsDuplicate.js
@@ -4,15 +4,23 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+// ---------------------- Less Efficient Solution
+// var containsDuplicate = function (nums) {
+//   let status = false;
+//   nums.forEach((val, idx) => {
+//     if (nums.indexOf(val) !== idx) {
+//       status = true;
+//     }
+//   })    
+//   return status;
+// };
+
+// ----------------------- More Efficient Solution
 var containsDuplicate = function (nums) {
-  let status = false;
-  nums.forEach((val, idx) => {
-    if (nums.indexOf(val) !== idx) {
-      status = true;
-    }
-  })    
-  return status;
+  let set = new Set(nums);
+  return set.size !== nums.length;
 };
+
 
 let nums = [1, 2, 3, 2]
 console.log(containsDuplicate(nums))
