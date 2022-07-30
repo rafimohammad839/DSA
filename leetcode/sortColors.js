31b5051e3e43a4e8261f7985997dfdccc097547f
Mohammad Rafi <mohammad.rafi@tothenew.com>
2022-07-30 18:57:41 +0530

    Leetcode 14: Longest Common Prefix

--- leetcode/sortColors.js
+++ leetcode/sortColors.js
@@ -3,6 +3,34 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
+
+// Less efficient | Two Pass | O(N + k)
+//  var sortColors = function (nums) {
+//   let n = nums.length;
+//   let red = 0;
+//   let white = 0;
+//   let blue = 0;
+//   for (let i = 0; i < nums.length; i++) {
+//       if (nums[i] === 0) {
+//           red++;
+//       } else if (nums[i] === 1) {
+//           white++;
+//       } else {
+//           blue++;
+//       }
+//   }
+//   for (let i = 0; i < red; i++) {
+//       nums[i] = 0;
+//   }
+//   for (let i = red; i < red + white; i++) {
+//       nums[i] = 1;
+//   }
+//   for (let i = red + white; i < n; i++) {
+//       nums[i] = 2;
+//   }
+// };
+
+// One Pass | Best Solution | Time - O(N)
 var sortColors = function (nums) {
   let start = 0;
   let end = nums.length - 1;
