51a046f6ac6b20dfbb09a16b8a985ee31755859a
Mohammad Rafi <mohammad.rafi@tothenew.com>
2022-07-30 17:21:03 +0530

    Leetcode 75: Sort Colors

---
+++ leetcode/sortColors.js
@@ -0,0 +1,28 @@
+// Leetcode 75: Sort Colors
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function (nums) {
+  let start = 0;
+  let end = nums.length - 1;
+  let i = 0;
+  while (i <= end) {
+    if (nums[i] === 0) {
+      nums[i] = nums[start]
+      nums[start] = 0;
+      i++;
+      start++;
+    } else if (nums[i] === 2) {
+      nums[i] = nums[end];
+      nums[end] = 2;
+      end--;
+    } else {
+      i++;
+    }
+  }
+};
+
+let nums = [2,0,2,1,1,0]
+sortColors(nums);
+console.log(nums);
