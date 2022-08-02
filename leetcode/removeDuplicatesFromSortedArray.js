60edb99e58e4fe1fc96fdf1d8929981dc11cd35b
Mohammad Rafi <rafi.mohammad@autodesk.com>
2022-08-02 20:53:31 +0530

    Leetcode: 26: Remove Duplicates from Sorted Array (Optimized)

--- leetcode/removeDuplicatesFromSortedArray.js
+++ leetcode/removeDuplicatesFromSortedArray.js
@@ -3,7 +3,8 @@
  * @param {number[]} nums
  * @return {number}
  */
-var removeDuplicates = function(nums) {
+// -------------------------------------- Solution 1 | Time - O(N) | Space - O(N)
+var removeDuplicates = function (nums) {
   let uniques = new Set();
   let i = 0;
   while (i < nums.length) {
@@ -15,6 +16,26 @@ var removeDuplicates = function(nums) {
     }
   }
   return nums.length;
+};
+
+// -------------------------------------- Solution 2 | Time - O(N) | Space - O(1)
+var removeDuplicates = function (nums) {
+  let left = 1,
+    index = 1,
+    len = nums.length;
+  // Keeping left pointer still if we encounter the same value as immediate previous value
+  while (index < len) {
+    if (nums[index] !== nums[index - 1]) {
+      nums[left] = nums[index];
+      left++;
+    }
+    index++;
+  }
+  // Starting from left pointer, filling undefined
+  for (let i = left; i < len; i++) {
+    nums[i] = undefined;
+  }
+  return nums;
 };
 
 let nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
