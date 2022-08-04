ea4a659edae561b1458d9433393199ecf3ad451d
Mohammad Rafi <rafi.mohammad@autodesk.com>
2022-08-04 11:38:55 +0530

    Intersection of Two Arrays

---
+++ leetcode/intersectionOfTwoArrays.js
@@ -0,0 +1,10 @@
+// Leetcode 349: Intersection of Two Arrays
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+//nums1 = [1,2,2,1], nums2 = [2,2]
+var intersection = function(nums1, nums2) {
+  
+};
