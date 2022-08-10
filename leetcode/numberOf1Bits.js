c078106cb6bc0e2807150f8f8237e302b2dc9085
Mohammad Rafi <rafi.mohammad@autodesk.com>
2022-08-10 10:30:27 +0530

    Leetcode 191: Number of 1 Bits

--- leetcode/numberOf1Bits.js
+++ leetcode/numberOf1Bits.js
@@ -3,12 +3,22 @@
  * @param {number} n - a positive integer
  * @return {number}
  */
-// ----------------------------------- Solution 1 | Less Efficient
- var hammingWeight = function(n) {
+// ----------------------------------- Solution 1 | Less Efficient | Time - O(1) | Need to check every bit
+//  var hammingWeight = function(n) {
+//   let count = 0;
+//   while (n) {
+//       count += n % 2;
+//       n = n >> 1;
+//   }
+//   return count;
+// };
+
+// ----------------------------------- Solution 2 | More Efficient | Time - O(1) | Need to check only one bits
+var hammingWeight = function(n) {
   let count = 0;
   while (n) {
-      count += n % 2;
-      n = n >> 1;
+      n = n & (n - 1);
+      count += 1;
   }
   return count;
 };
