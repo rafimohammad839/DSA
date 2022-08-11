7236baf3378b818f1c11a0c286e3211e3489f869
Mohammad Rafi <rafi.mohammad@autodesk.com>
2022-08-11 09:32:02 +0530

    Leetcode 347: Top K Frequent Elements (Optimized)

--- leetcode/topKFrequentElements.js
+++ leetcode/topKFrequentElements.js
@@ -4,94 +4,130 @@
  * @param {number} k
  * @return {number[]}
  */
- class PriorityQueue {
-  constructor() {
-    this.items = [];
-  }
+// -------------------------------------- Solution 1 | Space - O(N + k) | Time - O(NLogk)
+// class PriorityQueue {
+//   constructor() {
+//     this.items = [];
+//   }
 
-  heapify(i) {
-    let l = i * 2 + 1;
-    let r = i * 2 + 2;
-    let smallest = i;
-    if (l < this.items.length && this.items[l][1] < this.items[i][1]) {
-      smallest = l;
-    }
-    if (r < this.items.length && this.items[r][1] < this.items[smallest][1]) {
-      smallest = r;
-    }
-    if (i !== smallest) {
-      this.swap(i, smallest);
-      this.heapify(smallest);
-    }
-  }
-    
-  size() {
-      return this.items.length;
-  }
+//   heapify(i) {
+//     let l = i * 2 + 1;
+//     let r = i * 2 + 2;
+//     let smallest = i;
+//     if (l < this.items.length && this.items[l][1] < this.items[i][1]) {
+//       smallest = l;
+//     }
+//     if (r < this.items.length && this.items[r][1] < this.items[smallest][1]) {
+//       smallest = r;
+//     }
+//     if (i !== smallest) {
+//       this.swap(i, smallest);
+//       this.heapify(smallest);
+//     }
+//   }
 
-  bottomUpHeapify(i) {
-    let parent = Math.floor((i - 1) / 2);
-    if (i > 0) {
-      if (this.items[i][1] < this.items[parent][1]) {
-        this.swap(i, parent);
-        this.bottomUpHeapify(parent);
-      }
-    }
-  }
+//   size() {
+//     return this.items.length;
+//   }
 
-  swap(i, j) {
-    const arr = this.items;
-    let temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
-  }
+//   bottomUpHeapify(i) {
+//     let parent = Math.floor((i - 1) / 2);
+//     if (i > 0) {
+//       if (this.items[i][1] < this.items[parent][1]) {
+//         this.swap(i, parent);
+//         this.bottomUpHeapify(parent);
+//       }
+//     }
+//   }
 
-  remove() {
-    if (this.size() <= 0) return;
-     
-    if (this.size() === 1) {
-        this.items.pop();
-        return;
-    }
-      
-    this.swap(0, this.items.length - 1);
-    this.items.pop();
-    this.heapify(0);
-  }
+//   swap(i, j) {
+//     const arr = this.items;
+//     let temp = arr[i];
+//     arr[i] = arr[j];
+//     arr[j] = temp;
+//   }
 
-  insert(value) {
-    this.items.push(value);
-    this.bottomUpHeapify(this.items.length - 1);
-  }
-}
+//   remove() {
+//     if (this.size() <= 0) return;
 
+//     if (this.size() === 1) {
+//       this.items.pop();
+//       return;
+//     }
+
+//     this.swap(0, this.items.length - 1);
+//     this.items.pop();
+//     this.heapify(0);
+//   }
+
+//   insert(value) {
+//     this.items.push(value);
+//     this.bottomUpHeapify(this.items.length - 1);
+//   }
+// }
+
+// var topKFrequent = function (nums, k) {
+//   let buckets = new Map();
+//   let priorityQueue = new PriorityQueue();
+//   let len = nums.length;
+
+//   // Creating hashmap of frequency count
+//   for (let i = 0; i < len; i++) {
+//     if (buckets.has(nums[i])) {
+//       buckets.set(nums[i], buckets.get(nums[i]) + 1);
+//     } else {
+//       buckets.set(nums[i], 1);
+//     }
+//   }
+
+//   // Inserting elements in priorityQueue
+//   for (const [val, count] of buckets.entries()) {
+//     priorityQueue.insert([val, count]);
+//     if (priorityQueue.size() > k) {
+//       priorityQueue.remove();
+//     }
+//   }
+
+//   console.log("Priority Queue is", priorityQueue.items);
+
+//   // Getting the top k elements from priorityQueue
+//   return priorityQueue.items.map((val) => val[0]);
+// };
+
+//-------------------------------------- Solution 2 | Space - O(N) | Time - O(N)
 var topKFrequent = function (nums, k) {
-  let counts = new Map();
-  let priorityQueue = new PriorityQueue();
-  let len = nums.length;
-
-  // Creating hashmap of frequency count
-  for (let i = 0; i < len; i++) {
-    if (counts.has(nums[i])) {
-      counts.set(nums[i], counts.get(nums[i]) + 1);
+  let map = new Map();
+  let buckets = Array(nums.length + 1)
+    .fill(0)
+    .map(() => Array(0));
+  let results = [];
+  let counter = 0;
+  // Map for frequency count of each element
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(nums[i])) {
+      map.set(nums[i], map.get(nums[i]) + 1);
     } else {
-      counts.set(nums[i], 1);
+      map.set(nums[i], 1);
     }
   }
 
-  // Inserting elements in priorityQueue
-  for (const [val, count] of counts.entries()) {
-    priorityQueue.insert([val, count]);
-    if (priorityQueue.size() > k) {
-      priorityQueue.remove();
-    } 
+  // Applying bucket sort principle to insert elements according to their frequencies as indices
+  for (const [val, count] of map.entries()) {
+    buckets[count].push(val);
   }
 
-  console.log('Priority Queue is', priorityQueue.items);
-
-  // Getting the top k elements from priorityQueue
-  return priorityQueue.items.map((val) => val[0]);
+  // Looping through buckets array and getting elements from end
+  for (let i = buckets.length - 1; i >= 1 && results.length < k; i--) {
+    if (buckets[i].length) {
+      results.push(...buckets[i]);
+    }
+  }
+  return results;
 };
 
-let nums = [4, 1, -1, 2, -1, 2, 3], k = 2;
+let nums = [
+    3, 2, 3, 1, 2, 4, 5, 5, 6, 7, 7, 8, 2, 3, 1, 1, 1, 10, 11, 5, 6, 2, 4, 7, 8,
+    5, 6,
+  ],
+  k = 10;
 console.log(topKFrequent(nums, k));
