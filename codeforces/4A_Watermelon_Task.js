4bd7a688e4d87fc3e5806c31090a0d4e0eb3deba
Mohammad Rafi <rafi.mohammad@autodesk.com>
2022-08-11 10:55:30 +0530

    Initial commit

---
+++ codeforces/4A_Watermelon_Task.js
@@ -0,0 +1,10 @@
+function main() {
+    // const weight = readline();
+    const weight = 6;
+    if (weight % 2 === 0 && weight > 3) {
+        console.log("YES")
+    } else {
+        console.log("NO");
+    }
+}
+main();
