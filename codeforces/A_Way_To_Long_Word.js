97009538538f9405e24b3660df8f7632ac17e707
Mohammad Rafi <rafi.mohammad@autodesk.com>
2022-08-11 11:23:20 +0530

    71A - A Way Too Long Word

---
+++ codeforces/A_Way_To_Long_Word.js
@@ -0,0 +1,22 @@
+function main() {
+    let n = 4;
+    let words = ['word',
+        'localization',
+        'internationalization',
+        'pneumonoultramicroscopicsilicovolcanoconiosis'
+    ]
+    let result = Array(n);
+    for (let i = 0; i < n; i++) {
+        if (words[i].length > 10) {
+            let innerLength = words[i].length - 2;
+            let newWord = words[i][0] + innerLength + words[i][words[i].length-1];
+            result[i] = newWord;
+            console.log(result[i])
+        } else {
+            result[i] = words[i];
+            console.log(result[i]);
+        }
+    }
+}
+
+main();
