ae2eb374176f9ffa1ac933d663c2218b355330bc
Mohammad Rafi <rafi.mohammad@autodesk.com>
2022-08-13 13:38:51 +0530

    Leetcode 198: House Robber Optimized

--- leetcode/houseRobber.js
+++ leetcode/houseRobber.js
@@ -3,15 +3,26 @@
  * @param {number[]} nums
  * @return {number}
  */
-var rob = function (nums) {
-  let dp = Array(nums.length + 1).fill(0);
-  dp[0] = 0;
-  dp[1] = nums[0];
-  for (let i = 1; i < nums.length; i++) {
-    dp[i + 1] = Math.max(dp[i], dp[i - 1] + nums[i]);
-  }
-  return dp[nums.length];
-};
+// ----------------------------------- Solution 1 | Space - O(N) | Time - O(N)
+// var rob = function (nums) {
+//   let dp = Array(nums.length + 1).fill(0);
+//   dp[0] = 0;
+//   dp[1] = nums[0];
+//   for (let i = 1; i < nums.length; i++) {
+//     dp[i + 1] = Math.max(dp[i], dp[i - 1] + nums[i]);
+//   }
+//   return dp[nums.length];
+// };
 
-let nums = [1, 2, 2, 3, 100, 5, 8, 1];
+// ----------------------------------- Solution 2 | Space - O(1) | Time - O(N)
+var rob = function (nums) {
+  let first = 0, second = 0;
+  for (const i in nums) {
+    let temp = Math.max(second, first + nums[i]);
+    first = second;
+    second = temp;
+  }
+  return second;
+};
+let nums = [2,7,9,3,1];
 console.log(rob(nums));
