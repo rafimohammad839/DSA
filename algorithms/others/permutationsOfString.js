6dee9c0b8d6e405b25895feb4a4faceb17eb0010
Mohammad Rafi <rafi.mohammad@autodesk.com>
2022-08-15 11:52:27 +0530

    Permutations of String

---
+++ algorithms/others/permutationsOfString.js
@@ -0,0 +1,20 @@
+// Given a string, find all the permutations of the string
+function permutation(str) {
+  const permutations = [];
+  perm(str, "", permutations);
+  return permutations;
+}
+
+function perm(str, prefix, permutations) {
+  if (str.length === 0) {
+    permutations.push(prefix);
+    return;
+  }
+  for (let i = 0; i < str.length; i++) {
+    let pattern = str.substring(0, i) + str.substring(i + 1);
+    perm(pattern, prefix + str.charAt(i), permutations);
+  }
+}
+
+let string = "ABC";
+console.log(permutation(string));
