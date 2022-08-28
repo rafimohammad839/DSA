99da78f03f7fe431351a721d2d4601242067011b
Mohammad Rafi <rafi.mohammad@autodesk.com>
2022-08-28 13:48:02 +0530

    Recursion: String To Integer

--- algorithms/others/aoti_implementation.js
+++ algorithms/others/aoti_implementation.js
@@ -3,11 +3,10 @@ function strToInt(str, idx) {
   if (idx === 0) {
     return parseInt(str[0]);
   }
-  let ans = strToInt(str, idx - 1);
-  let res = ans * 10 + parseInt(str[idx]);
+  let res = strToInt(str, idx - 1) * 10 + parseInt(str[idx]);
   return res;
 }
 
-let str = "123456";
+let str = "53731";
 let idx = str.length - 1;
 console.log(strToInt(str, idx));
