3f8c21026bb2047c7a965dff17560b2220b74d40
Mohammad Rafi <rafi.mohammad@autodesk.com>
2022-09-03 20:06:34 +0530

    Leetcode 639: Decode Ways II

--- leetcode/decodeWaysII.js
+++ leetcode/decodeWaysII.js
@@ -3,10 +3,10 @@
  * @param {string} s
  * @return {number}
  */
-var numDecodings = function (s) {
-  let mod = Math.pow(10, 9) + 7;
+ var numDecodings = function (s) {
+  let mod = Math.pow(10, 9) + 7; 
+  let dp = {};
   let len = s.length;
-  let dp = Array(len + 1).fill(-1);
   let count = decode(s, 0, len, dp, mod);
   return count;
 };
@@ -18,59 +18,42 @@ function decode(s, i, len, dp, mod) {
   if (s[i] === "0") {
     return 0;
   }
-
-  if (dp[i] !== -1) {
+  if (i in dp) {
     return dp[i];
   }
-
-  let mult = 1, res = 0;
-
+  let mult = 1;
   if (s[i] === "*") {
     mult = 9;
   }
-
+  let res = 0;
   res += mult * decode(s, i + 1, len, dp, mod);
-
   if (i < len - 1) {
-    let next = s[i + 1];
     let opts = 0;
     if (s[i] === "*") {
-      if (next === "*") {
+      if (s[i + 1] === "*") {
         opts = 15;
+      } else if (parseInt(s[i + 1]) <= 6) {
+        opts = 2;
       } else {
-        if (parseInt(next) <= 6) {
-          opts = 2;
-        } else {
-          opts = 1;
-        }
+        opts = 1;
       }
-    }
-    
-    else if (parseInt(s[i]) < 3) {
-      if (next === "*") {
-        if (s[i] === "1") {
+    } else if (parseInt(s[i]) < 3) {
+      if (s[i + 1] === "*") {
+        if (parseInt(s[i]) === 1) {
           opts = 9;
-        }
-        else {
+        } else {
           opts = 6;
         }
-      }
-      else {
-        if (parseInt(s.substring(i, 2)) <= 26) {
-          opts = 1;
-        }
+      } else if (parseInt(s[i]+s[i+1]) <= 26) {
+        opts = 1;
       }
     }
-
-
     if (opts) {
       res += opts * decode(s, i + 2, len, dp, mod);
     }
 
   }
-  dp[i] = res % mod;
-  return dp[i];
+  return (dp[i] = res % mod);
 }
-
 let str = "1126";
 console.log(numDecodings(str));
