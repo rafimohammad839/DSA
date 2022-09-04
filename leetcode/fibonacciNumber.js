e680ab2b9f91f490e6cbe3a2868f826b90144e71
Mohammad Rafi <rafi.mohammad@autodesk.com>
2022-09-04 20:00:42 +0530

    Leetcode 509: Fibonacci Number (DP Solution added)

--- leetcode/fibonacciNumber.js
+++ leetcode/fibonacciNumber.js
@@ -3,14 +3,30 @@
  * @param {number} n
  * @return {number}
  */
- var fib = function (n, memo) {
-  memo = memo || {};
-  if (n in memo) return memo[n];
-  if (n <= 1) {
+// ------------------------------------------------------------ Solution 1 | Recursion with Memoization | Time - O(N) | Space - O(N)
+// var fib = function (n, memo) {
+//   memo = memo || {};
+//   if (n in memo) return memo[n];
+//   if (n <= 1) {
+//     return n;
+//   }
+//   memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+//   return memo[n];
+// };
+
+// ----------------------------------- Solution 2 | Dynamic Programming Solution | Time - O(N)
+var fib = function(n) {
+  if (n < 2) {
     return n;
   }
-  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
-  return memo[n];
+  let first = 0;
+  let second = 1;
+  for (let i = 2; i <= n; i++) {
+    let temp = second;
+    second = first + second;
+    first = temp;
+  }
+  return second;
 };
 
 console.time();
