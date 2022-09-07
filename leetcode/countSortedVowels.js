d6f14bc59eb2bc5dd7ac347d13a2ccd8ff2159c6
Mohammad Rafi <rafi.mohammad@autodesk.com>
2022-09-07 11:04:19 +0530

    Leetcode 1641: Count Sorted Vowel Strings (DP Solution added)

--- leetcode/countSortedVowels.js
+++ leetcode/countSortedVowels.js
@@ -3,25 +3,42 @@
  * @param {number} n
  * @return {number}
  */
+// ---------------------------------------------------------------- Solution 1 | Recursion | Time - O(5^N) | Space - O(N)
+// var countVowelStrings = function(n) {
+//   let count = 0;
+//   let vowels = ['a', 'e', 'i', 'o', 'u'];
+//   function dfs(n, l, vowels) {
+//     if (n === 0) {
+//       count++;
+//       return;
+//     }
+
+//     if (n < 0) {
+//       return;
+//     }
+
+//     for (let i = l; i < vowels.length; i++) {
+//       dfs(n - 1, i, vowels);
+//     }
+//   }
+//   dfs(n, 0, vowels);
+//   return count;
+// };
+
+// ---------------------------------------------------------------- Solution 2 | Dynamic Programming | Time - O(N) | Space - O(1)
 var countVowelStrings = function(n) {
-  let count = 0;
-  let vowels = ['a', 'e', 'i', 'o', 'u'];
-  function dfs(n, l, vowels) {
-    if (n === 0) {
-      count++;
-      return;
+  let dp = new Array(5).fill(1);
+  let sum = 0;
+  while (n > 1) {
+    for (let i = 1; i < dp.length; i++) {
+      dp[i] += dp[i - 1];
     }
-
-    if (n < 0) {
-      return;
-    }
-
-    for (let i = l; i < vowels.length; i++) {
-      dfs(n - 1, i, vowels);
-    }
+    n--;
   }
-  dfs(n, 0, vowels);
-  return count;
+  for (let i = 0; i < dp.length; i++) {
+    sum += dp[i];
+  }
+  return sum;
 };
 
 let n = 33;
