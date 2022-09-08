aa71cdd9c6e89a230ef4137215082ed0291fd6cc
Mohammad Rafi <rafi.mohammad@autodesk.com>
2022-09-08 10:21:39 +0530

    Prime Number: Two more solutions added

--- algorithms/others/primeNumber.js
+++ algorithms/others/primeNumber.js
@@ -1,4 +1,32 @@
 // Determine if a given number is prime
+
+
+// -------------------------------------------------------- Solution 1 | Bruteforce Solution | Time - O(N)
+// function isPrime(x) {
+//   let status = true;
+//   for (let i = 2; i < x; i++) {
+//     if (x % i === 0) {
+//       status = false;
+//       break;
+//     }
+//   }
+//   return status;
+// }
+
+// -------------------------------------------------------- Solution 2 | Less Bruteforce | Time - O(N/2)
+// function isPrime(x) {
+//   let status = true;
+//   for (let i = 2; i < x / 2; i++) {
+//     if (x % i === 0) {
+//       status = false;
+//       break;
+//     }
+//   }
+//   return status;
+// }
+
+
+// -------------------------------------------------------- Solution 3 | Optimal Solution | Time - O(√N)
 function isPrime(x) {
   let status = true;
   for (let i = 2; i * i <= x; i++) {
