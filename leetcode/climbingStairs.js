09951514a11c8a8613997522f778e200b29d6270
Mohammad Rafi <rafi.mohammad@autodesk.com>
2022-09-12 11:36:16 +0530

    Leetcode 70: Climbing Stairs (Two more solutions added)

--- leetcode/climbingStairs.js
+++ leetcode/climbingStairs.js
@@ -3,16 +3,48 @@
  * @param {number} n
  * @return {number}
  */
+
+// ------------------------------------------------------------ Solution 1 | Memoization
+// var climbStairs = function (n) {
+//   let dp = {};
+//   function climb(n, dp) {
+//     if (n === 1 || n === 2) {
+//       return n;
+//     }
+//     if (n in dp) return dp[n];
+//     return dp[n] = climb(n - 1, dp) + climb(n - 2, dp);
+//   }
+//   return climb(n, dp);
+// };
+
+
+// ------------------------------------------------------------ Solution 2 | 1D Array Dynamic Programming
+// var climbStairs = function (n) {
+//   let dp = new Array(n + 1).fill(0);
+//   dp[0] = 1;
+//   dp[1] = 1;
+//   for (let i = 2; i <= n; i++) {
+//     dp[i] = dp[i - 1] + dp[i - 2];
+//   }
+//   return dp[n];
+// };
+
+
+// ------------------------------------------------------------ Solution 3 | Constant Space Dynamic Programming
 var climbStairs = function (n) {
+  if (n === 1 || n === 2) {
+    return n;
+  }
   let first = 1;
   let second = 1;
-  for (let i = 0; i < n - 1; i++) {
-    let temp = first;
-    first = first + second;
-    second = temp;
+  for (let i = 2; i <= n; i++) {
+    let next = first + second;
+    first = second;
+    second = next;
   }
-  return first;
+  return second;
 };
-let n = 3;
+
+let n = 4;
 console.log(climbStairs(n));
 
