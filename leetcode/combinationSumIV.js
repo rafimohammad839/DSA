5c066eb9bd44186de9f073f48c7fe0b3146ccbf0
Mohammad Rafi <rafi.mohammad@autodesk.com>
2022-09-12 10:38:51 +0530

    Leetcode 377: Combination Sum IV

--- leetcode/combinationSumIV.js
+++ leetcode/combinationSumIV.js
@@ -4,27 +4,46 @@
  * @param {number} target
  * @return {number}
  */
+
+// --------------------------------------------------------------------- Solution 1 | Top Down DP
+// var combinationSum4 = function (nums, target) {
+//   let dp = {};
+//   function dfs(nums, target) {
+//     if (target === 0) {
+//       return 1;
+//     }
+
+//     if (target in dp) {
+//       return dp[target];
+//     }
+
+//     let count = 0;
+//     for (let i = 0; i < nums.length; i++) {
+//       if (nums[i] <= target) {
+//         count += dfs(nums, target - nums[i]);
+//       }
+//     }
+//     return  dp[target] = count;
+//   }  
+//   return dfs(nums, target);
+// };
+
+
+// --------------------------------------------------------------------- Solution 2 | Bottm Up DP
 var combinationSum4 = function (nums, target) {
-  let dp = {};
-  function dfs(nums, target) {
-    if (target === 0) {
-      return 1;
-    }
-
-    if (target in dp) {
-      return dp[target];
-    }
-
-    let count = 0;
-    for (let i = 0; i < nums.length; i++) {
-      if (nums[i] <= target) {
-        count += dfs(nums, target - nums[i]);
+  let len = nums.length;
+  let dp = new Array(target + 1).fill(0);
+  dp[0] = 1;
+  for (let i = 1; i < target + 1; i++) {
+    for (let j = 0; j < len; j++) {
+      if (nums[j] <= i) {
+        dp[i] += dp[i - nums[j]];
       }
     }
-    return  dp[target] = count;
-  }  
-  return dfs(nums, target);
+  }
+  return dp[target];
 };
+
 
 let nums = [1, 2, 3], target = 4;
 console.log(combinationSum4(nums, target));
