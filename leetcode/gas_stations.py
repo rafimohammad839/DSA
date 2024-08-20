9008be3006a671c293877ea7acdfa23dbfa86d12
Mohammad Rafi <mohammad.rafi@tothenew.com>
2024-08-20 15:13:32 +0530

    Diagonal Difference

--- leetcode/gas_stations.py
+++ leetcode/gas_stations.py
@@ -4,18 +4,18 @@ def canCompleteCircuit(gas, cost):
   if sum(gas) < sum(cost): return -1
 
   n = len(gas)
-  start, total = 0, 0
-
+  start = 0
+  total = 0
   for i in range(n):
     total += gas[i] - cost[i]
 
     if total < 0:
       total = 0
-      start = start + i
+      start = i + 1
   
   return start
   
-print(canCompleteCircuit(gas=[2,3,4], cost=[3,4,3]))
+print(canCompleteCircuit(gas = [2,3,4], cost = [3,4,3]))
 
 
 
