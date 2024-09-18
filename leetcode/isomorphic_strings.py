43db0a290de98c026b923184fa2476e5b48cc394
Mohammad Rafi <mohammad.rafi@tothenew.com>
2024-09-18 11:00:37 +0530

    Added Optimal Approach

--- leetcode/isomorphic_strings.py
+++ leetcode/isomorphic_strings.py
@@ -1,18 +1,34 @@
 # Leetcode 205. Isomorphic Strings
 
-def isIsomorphic(s: str, t: str) -> bool:
-  d = {}
+# Bruteforce Approach
+# def isIsomorphic(s: str, t: str) -> bool:
+#   d = {}
+#   n = len(s)
+
+#   for i in range(n):
+#     if s[i] not in d:
+#       for v in d.values():
+#         if v == t[i]:
+#           return False
+#       d[s[i]] = t[i]
+#     elif d[s[i]] != t[i]:
+#       return False
+  
+#   return True
+
+# print(isIsomorphic("egg", "add"))
+
+# Optimal Approach
+def isIsomorphic(s: str, t: str):
+  s_map = [0 for _ in range(128)]
+  t_map = [0 for _ in range(128)]
   n = len(s)
 
   for i in range(n):
-    if s[i] not in d:
-      for v in d.values():
-        if v == t[i]:
-          return False
-      d[s[i]] = t[i]
-    elif d[s[i]] != t[i]:
-      return False
+    if s_map[ord(s[i])] != t_map[ord(t[i])]: return False
+    s_map[ord(s[i])] = i + 1
+    t_map[ord(t[i])] = i + 1
   
   return True
 
-print(isIsomorphic("egg", "add"))
+print(isIsomorphic("add", "egg"))
